08a906fc5857a353c69ec821302d44a6af162044
jsoft <jmiguel.gomez@gmail.com>
2020-06-05 15:09:39 -0500

    VT12- AdminLTE III

--- app/Http/Controllers/Admin/permisoController.php
+++ app/Http/Controllers/Admin/permisoController.php
@@ -1,10 +1,12 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
+use App\Models\Admin\Permiso;
+use App\Http\Controllers\Controller;
 
-class PermisoController extends Controller
+class permisoController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -13,7 +15,9 @@ class PermisoController extends Controller
      */
     public function index()
     {
-        return "sistema";
+        $permisos = Permiso::orderBy('id')->get();
+        return view('admin.permiso.index', compact('permisos'));
+        //return view('admin.permiso.index');
     }
 
     /**
@@ -21,9 +25,9 @@ class PermisoController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function crear()
     {
-        //
+        return view('admin.permiso.crear');
     }
 
     /**
@@ -32,7 +36,7 @@ class PermisoController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function guardar(Request $request)
     {
         //
     }
@@ -43,7 +47,7 @@ class PermisoController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function mostrar($id)
     {
         //
     }
@@ -54,7 +58,7 @@ class PermisoController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function editar($id)
     {
         //
     }
@@ -66,7 +70,7 @@ class PermisoController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function actualizar(Request $request, $id)
     {
         //
     }
@@ -77,7 +81,7 @@ class PermisoController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function eliminar($id)
     {
         //
     }
